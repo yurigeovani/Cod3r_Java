ca23d55961c4f6a30597f5725625321ccf8487b1
Yuri Geovani <yuri_geovani@hotmail.com>
2023-01-17 17:54:01 +0000

    Until exercise 10 of TreeSet

--- src/exercisesAndTests/collections/treeSet/ex09/Ex09.java
+++ src/exercisesAndTests/collections/treeSet/ex09/Ex09.java
@@ -1,5 +1,7 @@
 package exercisesAndTests.collections.treeSet.ex09;
 
+import java.util.TreeSet;
+
 public class Ex09 {
 
 	public static void main(String[] args) {
@@ -8,7 +10,24 @@ public class Ex09 {
 		 * 9. Write a Java program to find the numbers less than 7 in a tree set.
 		 */
 		
+		TreeSet<Integer> treeNumbers = new TreeSet<>();
+		treeNumbers.add(8);
+		treeNumbers.add(5);
+		treeNumbers.add(2);
+		treeNumbers.add(7);
+		treeNumbers.add(10);
 		
+		System.out.println("Full tree: " + treeNumbers);
+		
+//		MY SOLUTION
+//		for (Integer number : treeNumbers) {
+//			if(number < 7) System.out.println(number);
+//		}
+		
+		TreeSet<Integer> treeheadset = new TreeSet<>();
+		treeheadset = (TreeSet<Integer>)treeNumbers.headSet(7);
+		
+		System.out.println(treeheadset);
 	}
 
 }
