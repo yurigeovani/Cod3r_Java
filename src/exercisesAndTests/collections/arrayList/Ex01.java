084b5be4eb466b646bf8d0a3d131faf3aba01fca
Yuri Geovani <yuri_geovani@hotmail.com>
2023-06-26 23:55:27 +0100

    Remembering arraylist ex01

---
+++ src/exercisesAndTests/collections/arrayList/Ex01.java
@@ -0,0 +1,23 @@
+package exercisesAndTests.collections.arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        /* 1. Write a Java program to create an array list, add some colors (strings) 
+        and print out the collection. */
+
+        List<String> listColors = new ArrayList<>();
+
+        listColors.add("Red");
+        listColors.add("Green");
+        listColors.add("Blue");
+        listColors.add("White");
+        listColors.add("Black");
+
+        System.out.println(listColors);
+
+    }
+}
