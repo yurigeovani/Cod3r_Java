ec5f8264f0a24746e833ecfbf7e538485968a8fa
Yuri Geovani <yuri_geovani@hotmail.com>
2024-05-22 21:30:00 +0100

    Until class 230

---
+++ test/s11_Project/com/ygtech/campoMinado/visao/Test.java
@@ -0,0 +1,21 @@
+package s11_Project.com.ygtech.campoMinado.visao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void testarSeIgualADois() {
+		int a = 1 + 1;
+		
+		assertEquals(2, a);
+	}
+	
+	@org.junit.jupiter.api.Test
+	void testarSeIgualATres() {
+		int x = 2 + 10 - 9;
+		
+		assertEquals(3, x);
+	}
+
+}
